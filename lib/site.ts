cbc14a830ed239b475a89f374e173054d1294066
SergeyShurekov <shurekov@gmail.com>
2026-09-21 12:42:55 +0300

    Fix production canonical URL

--- lib/site.ts
+++ lib/site.ts
@@ -1,4 +1,4 @@
-const DEFAULT_SITE_URL = "http://localhost:3000";
+const DEFAULT_SITE_URL = "https://www.hr-demandengine.ru";
 
 export function getSiteUrl(): string {
     return process.env.NEXT_PUBLIC_SITE_URL ?? DEFAULT_SITE_URL;
